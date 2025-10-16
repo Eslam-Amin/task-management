ae7ad8fa263d08147712139639994d2019a2c1f2
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-16 03:31:52 +0300

    Using Tasks services' function created create, getTasks, and getTask endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
-@Controller('tasks')
+@Controller({ path: 'tasks', version: '1' })
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() body: any) {
+    return this.tasksService.createTask(body.title, body.description);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
 }
