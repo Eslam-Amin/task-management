9f68b3c9601cb6c1cb91f63418a1e91a8cbdab6c
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-16 03:31:15 +0300

    Consuming the task model to an array of tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Task, TaskStatus } from './task.model';
 @Injectable()
 export class TasksService {
   constructor() {}
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) throw new NotFoundException(`Task with id: ${id} not found`);
+    return task;
+  }
 }
