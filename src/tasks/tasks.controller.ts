ded509a23b79431e3d408f20aa8ef0cc768c6906
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-18 16:58:47 +0300

    Used created task dto, and filter dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,57 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filte.dto';
 
 @Controller({ path: 'tasks', version: '1' })
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks() {
+  getTasks(@Query() filter: GetTasksFilterDto): Task[] {
+    if (Object.keys(filter))
+      return this.tasksService.getTasksWithFilters(filter);
     return this.tasksService.getAllTasks();
   }
 
   @Post()
-  createTask(@Body() body: any) {
-    return this.tasksService.createTask(body.title, body.description);
+  createTask(@Body() body: CreateTaskDto) {
+    return this.tasksService.createTask(body);
   }
 
   @Get('/:id')
   getTaskById(@Param('id') id: string) {
     return this.tasksService.getTaskById(id);
+  }
+
+  @Patch('/:id')
+  updateTaskById(
+    @Param('id') id: string,
+    @Body() body: Partial<CreateTaskDto>,
+  ) {
+    return this.tasksService.updateTaskById(id, body);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ) {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string) {
+    return this.tasksService.deleteTask(id);
   }
 }
