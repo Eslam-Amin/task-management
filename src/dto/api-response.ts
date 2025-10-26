c89902add25270e19758209a1d6ef213eaa2b8bb
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 00:16:18 +0300

    Created a global apiResponse

---
+++ src/dto/api-response.ts
@@ -0,0 +1,10 @@
+export class ApiResponse<T> {
+  pagination: {
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+    itemsPerPage: number;
+  };
+  message?: string;
+  data: T[];
+}
