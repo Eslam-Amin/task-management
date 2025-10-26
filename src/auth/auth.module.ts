409154ec24957ee4cec8bbab8a5f42b5985ebc6e
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 02:39:21 +0300

    Inject users repository in the module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { DataSource } from 'typeorm';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  imports: [],
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [
+    AuthService,
+    {
+      provide: 'USERS_REPOSITORY',
+      inject: [DataSource],
+      useFactory: (dataSource: DataSource) => UsersRepository(dataSource),
+    },
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
