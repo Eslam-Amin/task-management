a80ceda89560605f6ae363440e00fea4fba7fd50
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 03:29:01 +0300

    Created signup service function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@Inject('USERS_REPOSITORY') private usersRepository) {}
+  signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
+}
