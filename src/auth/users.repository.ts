68dcc746c78d3bb348f05c7bf1c774b2177b12e0
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-28 01:59:21 +0300

    Added Logger to the users repo in case of errors

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,13 +1,15 @@
 import { DataSource } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import { Logger } from '@nestjs/common';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
-export const UsersRepository = (dataSource: DataSource) =>
-  dataSource.getRepository(User).extend({
+export const UsersRepository = (dataSource: DataSource) => {
+  const logger = new Logger('UsersRepository', { timestamp: true });
+  return dataSource.getRepository(User).extend({
     async createUser(authCredentials: AuthCredentialsDto): Promise<User> {
       const { username, password } = authCredentials;
       const salt = await bcrypt.genSalt();
@@ -16,12 +18,16 @@ export const UsersRepository = (dataSource: DataSource) =>
       try {
         return await this.save(user);
       } catch (error) {
-        if (error.code === '23505')
+        if (error.code === '23505') {
+          logger.error(`Username "${username}" already exists.`);
           throw new ConflictException('Username already exists');
-        else
+        } else {
+          logger.error(`Failed to create user "${username}".`, error.stack);
           throw new InternalServerErrorException(
             'Failed to create user: ' + error.message,
           );
+        }
       }
     },
   });
+};
