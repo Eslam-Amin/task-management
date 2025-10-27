93eb0f5ccf0309a43d3d945d25097291bb810af5
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 03:28:39 +0300

    Created signup endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
+import { ApiResponse } from 'src/dto/api-response';
+import { User } from './user.entity';
 
-@Controller('auth')
-export class AuthController {}
+@Controller({ path: 'auth', version: '1' })
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<ApiResponse<User>> {
+    const user = await this.authService.signUp(authCredentialsDto);
+    return {
+      message: 'User created successfully',
+      data: user,
+    };
+  }
+}
