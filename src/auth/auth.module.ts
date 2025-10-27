5c3eed8481d23189fbd332b488e11fd473f21d8e
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 03:57:13 +0300

    import passport and jwt strategy for the accessToken generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { User } from './user.entity';
 import { DataSource } from 'typeorm';
 import { UsersRepository } from './users.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'supernotsosecret153',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [
     AuthService,
     {
