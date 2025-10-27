6bbf9877f7e40babe1ff26f43aa4514ab8e34c08
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-27 03:47:01 +0300

    Created signin service function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -7,5 +8,13 @@ export class AuthService {
   constructor(@Inject('USERS_REPOSITORY') private usersRepository) {}
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void | User> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ where: { username } });
+    if (user && (await bcrypt.compare(password, user.password)))
+      return 'Signed in successfully';
+    else throw new UnauthorizedException('Invalid credentials');
   }
 }
